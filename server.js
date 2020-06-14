c85ea6963651b789631cc298ff8cdae8030f560e
jadergermano <jader.germano@icloud.com>
2020-06-14 20:57:27 -0300

    Connection created woth mongoDb, creation of router, model and controller

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir")
 
 const app = express();
+app.use(express.json());
+
+const config = {
+    autoIndex: false,
+    useNewUrlParser: true,
+};
+
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi",
+    { config }
+);
+requireDir("./src/models");
+
+app.use("/api", require("./src/routes"));
 
 app.listen(3001);
---
+++ src/controllers/ProductController.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Product = mongoose.model("Product");
+
+module.exports = {
+    async index(req, res) {
+        const products = await Product.find();
+        return res.json(products);
+    },
+
+    async store(req, res) {
+        const product = await Product.create(req.body);
+        return res.json(product);
+    },
+
+    async show(req, res) {
+        const product = await Product.findOne(req.params);
+        return res.json(product);
+    },
+
+
+};
