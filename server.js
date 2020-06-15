6cbbe0ed53930862ea48512664e31458d90cb39e
jadergermano <jader.germano@icloud.com>
2020-06-14 21:29:01 -0300

    Adding Cors for controlling domain

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const requireDir = require("require-dir")
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 const config = {
     autoIndex: false,
     useNewUrlParser: true,
